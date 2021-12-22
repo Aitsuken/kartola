beff00fbf5c1918ce860565fa124f6ba31fd517b
Aitsuken <enderlordschannel@gmail.com>
2021-12-23 00:56:45 +0600

    kartola

---
+++ src/main/java/clyde/kartola/KartolaApplication.java
@@ -0,0 +1,22 @@
+package clyde.kartola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@RestController
+public class KartolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartolaApplication.class, args);
+    }
+    @GetMapping
+    public String Hello(){
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/clyde/kartola/KartolaApplicationTests.java
@@ -0,0 +1,13 @@
+package clyde.kartola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
