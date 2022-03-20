b95aa1c8563af1ad95ff139fa27ae2a5b3f05fba
Aitsuken <enderlordschannel@gmail.com>
2022-03-20 11:11:07 +0600

    experiments

--- src/main/java/clyde/kartola/KartolaApplication.java
+++ src/main/java/clyde/kartola/KartolaApplication.java
@@ -8,15 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @RestController
 public class KartolaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KartolaApplication.class, args);
     }
+
     @GetMapping
-    public String Hello(){
+    public String Hello() {
         return "Hello World!";
     }
 }
