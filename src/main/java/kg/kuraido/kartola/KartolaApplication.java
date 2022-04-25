8367894741b9eb83b6a34f83d71fcca6c5593dd3
aitsuken <enderlordschannel@gmail.com>
2022-04-25 14:59:47 +0600

    some changes

---
+++ src/main/java/kg/kuraido/kartola/KartolaApplication.java
@@ -0,0 +1,13 @@
+package kg.kuraido.kartola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartolaApplication.class, args);
+	}
+
+}
